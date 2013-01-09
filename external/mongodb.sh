84837c18aa69a215d9370bfc0fedd3485e3aa526
Michael Kuhn <michael.kuhn@informatik.uni-hamburg.de>
2013-01-09 09:27:14 +0100

    Update copyright dates.

--- external/mongodb.sh
+++ external/mongodb.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright (c) 2012 Michael Kuhn
+# Copyright (c) 2012-2013 Michael Kuhn
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
--- include/jconnection-internal.h
+++ include/jconnection-internal.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2012 Michael Kuhn
+ * Copyright (c) 2010-2013 Michael Kuhn
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
