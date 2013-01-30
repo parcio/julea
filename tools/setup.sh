81ca77b5b2f2f83d9001ff4eec2dc98d1254426c
Michael Kuhn <michael.kuhn@informatik.uni-hamburg.de>
2013-01-30 11:46:45 +0100

    Add setup tool.

---
+++ tools/setup.sh
@@ -0,0 +1,102 @@
+#!/bin/sh
+
+# Copyright (c) 2013 Michael Kuhn
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+set -e
+
+usage ()
+{
+	echo "Usage: $0 root start|stop"
+	exit 1
+}
+
+test -n "$1" || usage
+test -n "$2" || usage
+
+ROOT="$1"
+MODE="$2"
+
+BUILD="${ROOT}/build"
+MONGO_PATH='/tmp/julea-mongo'
+
+export PATH="${BUILD}/daemon:${BUILD}/tools:${ROOT}/external/mongodb-server/bin:${PATH}"
+export LD_LIBRARY_PATH="${BUILD}/lib"
+
+DATA=$(julea-config --local --print | grep ^data=)
+METADATA=$(julea-config --local --print | grep ^metadata=)
+
+DATA="${DATA#data=}"
+METADATA="${METADATA#metadata=}"
+
+DATA="${DATA%;}"
+METADATA="${METADATA%;}"
+
+DATA=$(echo ${DATA%;} | sed 's/;/ /g')
+METADATA=$(echo ${METADATA%;} | sed 's/;/ /g')
+
+HOSTNAME=$(hostname)
+
+for SERVER in ${DATA}
+do
+	HOST="${SERVER%:*}"
+	PORT="${SERVER#*:}"
+	test "${PORT}" = "${SERVER}" && PORT=''
+
+	if [ "${HOST}" = "${HOSTNAME}" ]
+	then
+		if [ "${MODE}" = 'start' ]
+		then
+			PORT_OPTION=''
+			test -n "${PORT}" && PORT_OPTION="--port ${PORT}"
+
+			julea-daemon --daemon ${PORT_OPTION}
+		elif [ "${MODE}" = 'stop' ]
+		then
+			killall julea-daemon || true
+		fi
+	fi
+done
+
+for SERVER in ${METADATA}
+do
+	HOST="${SERVER%:*}"
+	PORT="${SERVER#*:}"
+	test "${PORT}" = "${SERVER}" && PORT=''
+
+	if [ "${HOST}" = "${HOSTNAME}" ]
+	then
+		if [ "${MODE}" = 'start' ]
+		then
+			mkdir -p "${MONGO_PATH}"
+			mkdir -p "${MONGO_PATH}/db"
+
+			mongod --fork --logpath "${MONGO_PATH}/mongod.log" --logappend --dbpath "${MONGO_PATH}/db" --journal
+		elif [ "${MODE}" = 'stop' ]
+		then
+			mongod --shutdown --logpath "${MONGO_PATH}/mongod.log" --logappend --dbpath "${MONGO_PATH}/db" --journal || true
+		fi
+	fi
+done
