646e9782dadcfc488ec504ef9d0ecc2233af6c3b
Michael Kuhn <michael.kuhn@informatik.uni-hamburg.de>
2013-02-26 16:02:35 +0100

    Add MPI-I/O and POSIX support to small access benchmark.

--- benchmarks/small-access.c
+++ benchmarks/small-access.c
@@ -25,6 +25,8 @@
  * SUCH DAMAGE.
  */
 
+#define _POSIX_C_SOURCE 200809L
+
 #include <julea-config.h>
 
 #include <glib.h>
@@ -40,10 +42,19 @@
 #include <mpi.h>
 #endif
 
-static gchar* opt_semantics = NULL;
-static gchar* opt_template = NULL;
+static gboolean opt_julea = FALSE;
+static gboolean opt_mpi_io = FALSE;
+static gboolean opt_posix = FALSE;
 
-static JSemantics* semantics = NULL;
+static gint opt_block_size = 4096;
+static gboolean opt_shared = FALSE;
+
+static gchar* opt_julea_semantics = NULL;
+static gchar* opt_julea_template = NULL;
+
+static gchar* opt_mpi_io_path = NULL;
+
+static gchar* opt_posix_path = NULL;
 
 static guint kill_thread = 0;
 
@@ -103,22 +114,80 @@ print_statistics (G_GNUC_UNUSED gpointer user_data)
 }
 
 static
-gpointer
-benchmark_thread (G_GNUC_UNUSED gpointer data)
+guint64
+get_offset (guint64 iteration)
 {
-	guint64 const block_size = 4096;
-	gchar buf[block_size];
+	if (opt_shared)
+	{
+		return opt_block_size * iteration;
+	}
+	else
+	{
+		return opt_block_size * ((process_num * iteration) + process_id);
+	}
+}
+
+static
+gchar const*
+get_name (void)
+{
+	static gchar* name = NULL;
+
+	if (G_UNLIKELY(name == NULL))
+	{
+		if (opt_shared)
+		{
+			name = g_strdup("benchmark");
+		}
+		else
+		{
+			name = g_strdup_printf("benchmark%d", process_id);
+		}
+	}
+
+	return name;
+}
+
+static
+gpointer
+thread_julea (G_GNUC_UNUSED gpointer data)
+{
+	gchar buf[opt_block_size];
 
 	JBatch* batch;
 	JCollection* collection;
 	JItem* item;
+	JSemantics* semantics;
 	JStore* store;
-	guint64 offset = 0;
+	guint64 iteration = 0;
+
+	j_init(NULL, NULL);
+
+	if (!opt_shared || process_id == 0)
+	{
+		batch = j_batch_new_for_template(J_SEMANTICS_TEMPLATE_DEFAULT);
+
+		store = j_store_new("benchmark");
+		collection = j_collection_new("benchmark");
+		item = j_item_new(get_name());
+
+		j_create_store(store, batch);
+		j_store_create_collection(store, collection, batch);
+		j_collection_create_item(collection, item, batch);
+
+		j_item_unref(item);
+		j_collection_unref(collection);
+		j_store_unref(store);
+
+		j_batch_execute(batch);
+		j_batch_unref(batch);
+	}
 
 #ifdef HAVE_MPI
 	MPI_Barrier(MPI_COMM_WORLD);
 #endif
 
+	semantics = j_helper_parse_semantics(opt_julea_template, opt_julea_semantics);
 	batch = j_batch_new(semantics);
 
 	j_get_store(&store, "benchmark", batch);
@@ -127,7 +196,7 @@ benchmark_thread (G_GNUC_UNUSED gpointer data)
 	j_store_get_collection(store, &collection, "benchmark", batch);
 	j_batch_execute(batch);
 
-	j_collection_get_item(collection, &item, "benchmark", batch);
+	j_collection_get_item(collection, &item, get_name(), batch);
 	j_batch_execute(batch);
 
 	if (item == NULL)
@@ -146,7 +215,7 @@ benchmark_thread (G_GNUC_UNUSED gpointer data)
 		{
 			guint64 bytes;
 
-			j_item_write(item, buf, block_size, block_size * ((process_num * (offset + i)) + process_id), &bytes, batch);
+			j_item_write(item, buf, opt_block_size, get_offset(iteration + i), &bytes, batch);
 			j_batch_execute(batch);
 
 			g_atomic_int_add(&stat_write, bytes);
@@ -161,7 +230,7 @@ benchmark_thread (G_GNUC_UNUSED gpointer data)
 			guint64 bytes;
 			gint other_process = (process_id + 1) % process_num;
 
-			j_item_read(item, buf, block_size, block_size * ((process_num * (offset + i)) + other_process), &bytes, batch);
+			j_item_read(item, buf, opt_block_size, opt_block_size * ((process_num * (iteration + i)) + other_process), &bytes, batch);
 			j_batch_execute(batch);
 
 			g_atomic_int_add(&stat_read, bytes);
@@ -176,7 +245,7 @@ benchmark_thread (G_GNUC_UNUSED gpointer data)
 			guint64 bytes;
 			gint other_process = (process_id + 1) % process_num;
 
-			j_item_write(item, buf, block_size, block_size * ((process_num * (offset + i)) + other_process), &bytes, batch);
+			j_item_write(item, buf, opt_block_size, opt_block_size * ((process_num * (iteration + i)) + other_process), &bytes, batch);
 			j_batch_execute(batch);
 
 			g_atomic_int_add(&stat_update, bytes);
@@ -186,15 +255,221 @@ benchmark_thread (G_GNUC_UNUSED gpointer data)
 		MPI_Barrier(MPI_COMM_WORLD);
 #endif
 
-		offset += 1000;
+		iteration += 1000;
 	}
 
 end:
+	j_batch_unref(batch);
+
+	j_semantics_unref(semantics);
+
+	if (!opt_shared || process_id == 0)
+	{
+		batch = j_batch_new_for_template(J_SEMANTICS_TEMPLATE_DEFAULT);
+
+		j_collection_delete_item(collection, item, batch);
+		j_store_delete_collection(store, collection, batch);
+		j_delete_store(store, batch);
+
+		j_batch_execute(batch);
+		j_batch_unref(batch);
+	}
+
 	j_store_unref(store);
 	j_collection_unref(collection);
 	j_item_unref(item);
 
-	j_batch_unref(batch);
+	j_fini();
+
+	return NULL;
+}
+
+static
+gpointer
+thread_mpi_io (G_GNUC_UNUSED gpointer data)
+{
+#ifdef HAVE_MPI
+	gchar buf[opt_block_size];
+
+	MPI_Comm comm;
+	MPI_File file[1];
+	MPI_Status status[1];
+	gchar* path;
+	guint64 iteration = 0;
+	gint ret;
+
+	comm = (opt_shared) ? MPI_COMM_WORLD : MPI_COMM_SELF;
+
+	if (!opt_shared || process_id == 0)
+	{
+		path = g_build_filename(opt_mpi_io_path, get_name(), NULL);
+		MPI_File_open(comm, path, MPI_MODE_RDWR | MPI_MODE_CREATE, MPI_INFO_NULL, file);
+
+		MPI_File_close(file);
+		g_free(path);
+	}
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	path = g_build_filename(opt_posix_path, get_name(), NULL);
+	ret = MPI_File_open(comm, path, MPI_MODE_RDWR, MPI_INFO_NULL, file);
+
+	if (ret != MPI_SUCCESS)
+	{
+		g_error("ERROR %d\n", process_id);
+	}
+
+	while (TRUE)
+	{
+		if (g_atomic_int_get(&kill_thread) == 1)
+		{
+			goto end;
+		}
+
+		for (guint i = 0; i < 1000; i++)
+		{
+			gint bytes;
+
+			MPI_File_write_at(file[0], get_offset(iteration + i), buf, opt_block_size, MPI_CHAR, status);
+			MPI_Get_count(status, MPI_CHAR, &bytes);
+
+			g_atomic_int_add(&stat_write, bytes);
+		}
+
+		MPI_Barrier(MPI_COMM_WORLD);
+
+		for (guint i = 0; i < 1000; i++)
+		{
+			gint bytes;
+			gint other_process = (process_id + 1) % process_num;
+
+			MPI_File_write_at(file[0], opt_block_size * ((process_num * (iteration + i)) + other_process), buf, opt_block_size, MPI_CHAR, status);
+			MPI_Get_count(status, MPI_CHAR, &bytes);
+
+			g_atomic_int_add(&stat_read, bytes);
+		}
+
+		MPI_Barrier(MPI_COMM_WORLD);
+
+		for (guint i = 0; i < 1000; i++)
+		{
+			gint bytes;
+			gint other_process = (process_id + 1) % process_num;
+
+			MPI_File_write_at(file[0], opt_block_size * ((process_num * (iteration + i)) + other_process), buf, opt_block_size, MPI_CHAR, status);
+			MPI_Get_count(status, MPI_CHAR, &bytes);
+
+			g_atomic_int_add(&stat_update, bytes);
+		}
+
+		MPI_Barrier(MPI_COMM_WORLD);
+
+		iteration += 1000;
+	}
+
+end:
+	if (!opt_shared || process_id == 0)
+	{
+		unlink(path);
+	}
+
+	MPI_File_close(file);
+	g_free(path);
+
+#endif
+	return NULL;
+}
+
+static
+gpointer
+thread_posix (G_GNUC_UNUSED gpointer data)
+{
+	gchar buf[opt_block_size];
+
+	gchar* path;
+	gint fd;
+	guint64 iteration = 0;
+
+	if (!opt_shared || process_id == 0)
+	{
+		path = g_build_filename(opt_posix_path, get_name(), NULL);
+		fd = open(path, O_RDWR | O_CREAT, 0600);
+
+		close(fd);
+		g_free(path);
+	}
+
+#ifdef HAVE_MPI
+	MPI_Barrier(MPI_COMM_WORLD);
+#endif
+
+	path = g_build_filename(opt_posix_path, get_name(), NULL);
+	fd = open(path, O_RDWR);
+
+	if (fd == -1)
+	{
+		g_error("ERROR %d\n", process_id);
+	}
+
+	while (TRUE)
+	{
+		if (g_atomic_int_get(&kill_thread) == 1)
+		{
+			goto end;
+		}
+
+		for (guint i = 0; i < 1000; i++)
+		{
+			guint64 bytes;
+
+			bytes = pwrite(fd, buf, opt_block_size, get_offset(iteration + i));
+
+			g_atomic_int_add(&stat_write, bytes);
+		}
+
+#ifdef HAVE_MPI
+		MPI_Barrier(MPI_COMM_WORLD);
+#endif
+
+		for (guint i = 0; i < 1000; i++)
+		{
+			guint64 bytes;
+			gint other_process = (process_id + 1) % process_num;
+
+			bytes = pread(fd, buf, opt_block_size, opt_block_size * ((process_num * (iteration + i)) + other_process));
+
+			g_atomic_int_add(&stat_read, bytes);
+		}
+
+#ifdef HAVE_MPI
+		MPI_Barrier(MPI_COMM_WORLD);
+#endif
+
+		for (guint i = 0; i < 1000; i++)
+		{
+			guint64 bytes;
+			gint other_process = (process_id + 1) % process_num;
+
+			bytes = pwrite(fd, buf, opt_block_size, opt_block_size * ((process_num * (iteration + i)) + other_process));
+
+			g_atomic_int_add(&stat_update, bytes);
+		}
+
+#ifdef HAVE_MPI
+		MPI_Barrier(MPI_COMM_WORLD);
+#endif
+
+		iteration += 1000;
+	}
+
+end:
+	if (!opt_shared || process_id == 0)
+	{
+		unlink(path);
+	}
+
+	close(fd);
+	g_free(path);
 
 	return NULL;
 }
@@ -204,19 +479,37 @@ main (int argc, char** argv)
 {
 	GError* error = NULL;
 	GOptionContext* context;
+	GOptionGroup* group;
 
 	GOptionEntry entries[] = {
-		{ "semantics", 0, 0, G_OPTION_ARG_STRING, &opt_semantics, "Semantics to use", NULL },
-		{ "template", 0, 0, G_OPTION_ARG_STRING, &opt_template, "Semantics template to use", "default" },
+		{ "julea", 0, 0, G_OPTION_ARG_NONE, &opt_julea, "Use JULEA I/O", NULL },
+#ifdef HAVE_MPI
+		{ "mpi-io", 0, 0, G_OPTION_ARG_NONE, &opt_mpi_io, "Use MPI-I/O", NULL },
+#endif
+		{ "posix", 0, 0, G_OPTION_ARG_NONE, &opt_posix, "Use POSIX I/O", NULL },
+		{ "block-size", 0, 0, G_OPTION_ARG_INT, &opt_block_size, "Block size", "4096" },
+		{ "shared", 0, 0, G_OPTION_ARG_NONE, &opt_shared, "Use shared access", NULL },
 		{ NULL, 0, 0, 0, NULL, NULL, NULL }
 	};
 
-	JBatch* batch;
-	JCollection* collection;
-	JItem* item;
-	JStore* store;
+	GOptionEntry entries_julea[] = {
+		{ "julea-semantics", 0, 0, G_OPTION_ARG_STRING, &opt_julea_semantics, "Semantics to use", NULL },
+		{ "julea-template", 0, 0, G_OPTION_ARG_STRING, &opt_julea_template, "Semantics template to use", NULL },
+		{ NULL, 0, 0, 0, NULL, NULL, NULL }
+	};
+
+	GOptionEntry entries_mpi_io[] = {
+		{ "mpi-io-path", 0, 0, G_OPTION_ARG_STRING, &opt_mpi_io_path, "Path to use", NULL },
+		{ NULL, 0, 0, 0, NULL, NULL, NULL }
+	};
+
+	GOptionEntry entries_posix[] = {
+		{ "posix-path", 0, 0, G_OPTION_ARG_STRING, &opt_posix_path, "Path to use", NULL },
+		{ NULL, 0, 0, 0, NULL, NULL, NULL }
+	};
+
 	GMainLoop* main_loop;
-	GThread* thread;
+	GThread* thread = NULL;
 
 #ifdef HAVE_MPI
 	{
@@ -237,6 +530,18 @@ main (int argc, char** argv)
 	context = g_option_context_new(NULL);
 	g_option_context_add_main_entries(context, entries, NULL);
 
+	group = g_option_group_new("julea", "JULEA Options", "Show JULEA help options", NULL, NULL);
+	g_option_group_add_entries(group, entries_julea);
+	g_option_context_add_group(context, group);
+
+	group = g_option_group_new("mpi-io", "MPI-I/O Options", "Show MPI-I/O help options", NULL, NULL);
+	g_option_group_add_entries(group, entries_mpi_io);
+	g_option_context_add_group(context, group);
+
+	group = g_option_group_new("posix", "POSIX Options", "Show POSIX help options", NULL, NULL);
+	g_option_group_add_entries(group, entries_posix);
+	g_option_context_add_group(context, group);
+
 	if (!g_option_context_parse(context, &argc, &argv, &error))
 	{
 		g_option_context_free(context);
@@ -250,28 +555,36 @@ main (int argc, char** argv)
 		return 1;
 	}
 
-	g_option_context_free(context);
-
-	semantics = j_helper_parse_semantics(opt_template, opt_semantics);
-
-	j_init(&argc, &argv);
-
-	if (process_id == 0)
+	if ((!opt_julea && !opt_mpi_io && !opt_posix)
+	    || (opt_mpi_io && opt_mpi_io_path == NULL)
+	    || (opt_posix && opt_posix_path == NULL)
+	)
 	{
-		batch = j_batch_new_for_template(J_SEMANTICS_TEMPLATE_DEFAULT);
+		gchar* help;
 
-		store = j_store_new("benchmark");
-		collection = j_collection_new("benchmark");
-		item = j_item_new("benchmark");
+		help = g_option_context_get_help(context, TRUE, NULL);
+		g_option_context_free(context);
 
-		j_create_store(store, batch);
-		j_store_create_collection(store, collection, batch);
-		j_collection_create_item(collection, item, batch);
+		g_print("%s", help);
+		g_free(help);
 
-		j_batch_execute(batch);
+		return 1;
 	}
 
-	thread = g_thread_new("test", benchmark_thread, NULL);
+	g_option_context_free(context);
+
+	if (opt_julea)
+	{
+		thread = g_thread_new("test", thread_julea, NULL);
+	}
+	else if (opt_mpi_io)
+	{
+		thread = g_thread_new("test", thread_mpi_io, NULL);
+	}
+	else if (opt_posix)
+	{
+		thread = g_thread_new("test", thread_posix, NULL);
+	}
 
 	main_loop = g_main_loop_new(NULL, FALSE);
 	g_timeout_add_seconds(1, print_statistics, NULL);
@@ -284,26 +597,12 @@ main (int argc, char** argv)
 
 	g_atomic_int_set(&kill_thread, 1);
 
-	g_thread_join(thread);
-
-	g_main_loop_unref(main_loop);
-
-	if (process_id == 0)
+	if (thread != NULL)
 	{
-		j_collection_delete_item(collection, item, batch);
-		j_store_delete_collection(store, collection, batch);
-		j_delete_store(store, batch);
-
-		j_item_unref(item);
-		j_collection_unref(collection);
-		j_store_unref(store);
-
-		j_batch_execute(batch);
-
-		j_batch_unref(batch);
+		g_thread_join(thread);
 	}
 
-	j_fini();
+	g_main_loop_unref(main_loop);
 
 #ifdef HAVE_MPI
 	MPI_Finalize();
