9d23c181a47b124605d2185c19c6e9500dab20c6
Michael Kuhn <michael.kuhn@informatik.uni-hamburg.de>
2013-03-19 10:05:04 +0100

    Overhaul benchmark script.

--- tools/benchmark.sh
+++ tools/benchmark.sh
@@ -1,79 +1,105 @@
-#!/bin/bash
+#!/bin/sh
 
-cd ..
+# Copyright (c) 2013 Christina Janssen
+# Copyright (c) 2013 Michael Kuhn
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+set -e
+
+usage ()
+{
+	echo "Usage: ${0##*/} root"
+	exit 1
+}
+
+test -n "$1" || usage
+
+ROOT="$1"
+
+BUILD_PATH="${ROOT}/build"
+
+export PATH="${BUILD_PATH}/benchmark:${ROOT}/tools:${PATH}"
+export LD_LIBRARY_PATH="${BUILD_PATH}/lib:${LD_LIBRARY_PATH}"
+
+TEMP=$(mktemp -d --tmpdir="${PWD}")
+
+trap "setup.sh ${ROOT} stop" HUP INT TERM 0
+
+echo "Writing results to: ${TEMP}"
+cd "${TEMP}"
 
 echo Templates
-./tools/setup.sh $PWD start
-./build/benchmark/benchmark > benchmark-template-default.log
-./tools/setup.sh $PWD stop
 
-./tools/setup.sh $PWD start
-./build/benchmark/benchmark --template posix > benchmark-template-posix.log
-./tools/setup.sh $PWD stop
-
-./tools/setup.sh $PWD start
-./build/benchmark/benchmark --template checkpoint > benchmark-template-checkpoint.log
-./tools/setup.sh $PWD stop
-
-./tools/setup.sh $PWD start
-./build/benchmark/benchmark --template serial > benchmark-template-serial.log
-./tools/setup.sh $PWD stop
-
+for template in default posix checkpoint serial
+do
+	setup.sh "${ROOT}" start
+	benchmark --template "${template}" > "benchmark-template-${template}.log"
+	setup.sh "${ROOT}" stop
+done
 
 echo Atomicity
-./tools/setup.sh $PWD start
-./build/benchmark/benchmark --semantics atomicity=operation > benchmark-atomicity-operation.log
-./tools/setup.sh $PWD stop
 
-./tools/setup.sh $PWD start
-./build/benchmark/benchmark --semantics atomicity=none > benchmark-atomicity-none.log
-./tools/setup.sh $PWD stop
-
+for atomicity in operation none
+do
+	setup.sh "${ROOT}" start
+	benchmark --semantics atomicity="${atomicity}" > "benchmark-atomicity-${atomicity}.log"
+	setup.sh "${ROOT}" stop
+done
 
 echo Concurrency
-./tools/setup.sh $PWD start
-./build/benchmark/benchmark --semantics concurrency=overlapping > benchmark-concurrency-overlapping.log
-./tools/setup.sh $PWD stop
 
-./tools/setup.sh $PWD start
-./build/benchmark/benchmark --semantics concurrency=non-overlapping > benchmark-concurrency-nonoverlapping.log
-./tools/setup.sh $PWD stop
-
-./tools/setup.sh $PWD start
-./build/benchmark/benchmark --semantics concurrency=none > benchmark-concurrency-none.log
-./tools/setup.sh $PWD stop
-
+for concurrency in overlapping non-overlapping none
+do
+	setup.sh "${ROOT}" start
+	benchmark --semantics concurrency="${concurrency}" > "benchmark-concurrency-${concurrency}.log"
+	setup.sh "${ROOT}" stop
+done
 
 echo Consistency
-./tools/setup.sh $PWD start
-./build/benchmark/benchmark --semantics consistency=immediate > benchmark-consistency-immediate.log
-./tools/setup.sh $PWD stop
 
-./tools/setup.sh $PWD start
-./build/benchmark/benchmark --semantics consistency=eventual > benchmark-consistency-eventual.log
-./tools/setup.sh $PWD stop
-
+for consistency in immediate eventual
+do
+	setup.sh "${ROOT}" start
+	benchmark --semantics consistency="${consistency}" > "benchmark-consistency-${consistency}.log"
+	setup.sh "${ROOT}" stop
+done
 
 echo Persistency
-./tools/setup.sh $PWD start
-./build/benchmark/benchmark --semantics persistency=immediate > benchmark-persistency-immediate.log
-./tools/setup.sh $PWD stop
 
-./tools/setup.sh $PWD start
-./build/benchmark/benchmark --semantics persistency=eventual > benchmark-persistency-eventual.log
-./tools/setup.sh $PWD stop
-
+for persistency in immediate eventual
+do
+	setup.sh "${ROOT}" start
+	benchmark --semantics persistency="${persistency}" > "benchmark-persistency-${persistency}.log"
+	setup.sh "${ROOT}" stop
+done
 
 echo Safety
-#./tools/setup.sh $PWD start
-#./build/benchmark/benchmark --semantics safety=storage > benchmark-safety-storage.log
-#./tools/setup.sh $PWD stop
 
-./tools/setup.sh $PWD start
-./build/benchmark/benchmark --semantics safety=network > benchmark-safety-network.log
-./tools/setup.sh $PWD stop
-
-./tools/setup.sh $PWD start
-./build/benchmark/benchmark --semantics safety=none > benchmark-safety-none.log
-./tools/setup.sh $PWD stop
-
+for safety in storage network none
+do
+	setup.sh "${ROOT}" start
+	benchmark --semantics safety="${safety}" > "benchmark-safety-${safety}.log"
+	setup.sh "${ROOT}" stop
+done
