7267a2deb7f362d3ab6d58ee0e36d128421abbe5
Michael Kuhn <michael.kuhn@informatik.uni-hamburg.de>
2019-07-25 12:23:07 +0200

    Fix distribution test and setup script

--- test/distribution.c
+++ test/distribution.c
@@ -40,12 +40,16 @@ test_distribution_fixture_setup (JConfiguration** configuration, gconstpointer d
 	key_file = g_key_file_new();
 	g_key_file_set_string_list(key_file, "servers", "object", servers, 2);
 	g_key_file_set_string_list(key_file, "servers", "kv", servers, 2);
+	g_key_file_set_string_list(key_file, "servers", "db", servers, 2);
 	g_key_file_set_string(key_file, "object", "backend", "null");
 	g_key_file_set_string(key_file, "object", "component", "server");
 	g_key_file_set_string(key_file, "object", "path", "");
 	g_key_file_set_string(key_file, "kv", "backend", "null");
 	g_key_file_set_string(key_file, "kv", "component", "server");
 	g_key_file_set_string(key_file, "kv", "path", "");
+	g_key_file_set_string(key_file, "db", "backend", "null");
+	g_key_file_set_string(key_file, "db", "component", "server");
+	g_key_file_set_string(key_file, "db", "path", "");
 
 	*configuration = j_configuration_new_for_data(key_file);
 
