8b1f0c7c84f413240e2bbb88446862cccf9553c2
Benjamin Warnke <4bwarnke@informatik.uni-hamburg.de>
2019-08-16 16:40:53 +0200

    suppress warnings by clang

--- include/core/jtrace.h
+++ include/core/jtrace.h
@@ -59,11 +59,11 @@ void j_trace_leave (JTrace*);
 G_DEFINE_AUTOPTR_CLEANUP_FUNC(JTrace, j_trace_leave)
 
 #ifdef __COUNTER__
-#define J_TRACE(name, ...) g_autoptr(JTrace) G_PASTE(j_trace, __COUNTER__) G_GNUC_UNUSED = j_trace_enter(name, __VA_ARGS__)
-#define J_TRACE_FUNCTION(...) g_autoptr(JTrace) G_PASTE(j_trace_function, __COUNTER__) G_GNUC_UNUSED = j_trace_enter(G_STRFUNC, __VA_ARGS__)
+#define J_TRACE(name, ...) __attribute__((unused)) g_autoptr(JTrace) G_PASTE(j_trace, __COUNTER__) G_GNUC_UNUSED = j_trace_enter(name, __VA_ARGS__)
+#define J_TRACE_FUNCTION(...) __attribute__((unused)) g_autoptr(JTrace) G_PASTE(j_trace_function, __COUNTER__) G_GNUC_UNUSED = j_trace_enter(G_STRFUNC, __VA_ARGS__)
 #else
-#define J_TRACE(name, ...) g_autoptr(JTrace) G_PASTE(j_trace, __LINE__) G_GNUC_UNUSED = j_trace_enter(name, __VA_ARGS__)
-#define J_TRACE_FUNCTION(...) g_autoptr(JTrace) G_PASTE(j_trace_function, __LINE__) G_GNUC_UNUSED = j_trace_enter(G_STRFUNC, __VA_ARGS__)
+#define J_TRACE(name, ...) __attribute__((unused)) g_autoptr(JTrace) G_PASTE(j_trace, __LINE__) G_GNUC_UNUSED = j_trace_enter(name, __VA_ARGS__)
+#define J_TRACE_FUNCTION(...) __attribute__((unused)) g_autoptr(JTrace) G_PASTE(j_trace_function, __LINE__) G_GNUC_UNUSED = j_trace_enter(G_STRFUNC, __VA_ARGS__)
 #endif
 
 void j_trace_file_begin (gchar const*, JTraceFileOperation);
