2cca7cffc7fb111a5fce1d4b72f0a1f515d04f34
Arnatar <arnestruck@astruck.de>
2020-03-13 19:49:05 +0100

    added domain_manager functionalities to jconnection-pool

--- lib/core/jconnection-pool.c
+++ lib/core/jconnection-pool.c
@@ -46,6 +46,8 @@
 
 #include <glib/gprintf.h>
 
+#include <j_fi_domain_manager.h>
+
 /**
  * \defgroup JConnectionPool Connection Pool
  *
@@ -83,15 +85,11 @@ static JConnectionPool* j_connection_pool = NULL;
 
 //libfabric high level objects
 static struct fid_fabric* j_fabric;
-static struct fid_domain* j_domain;
-static struct fid_eq* j_domain_event_queue;
+
 //libfabric config structures
 static struct fi_info* j_info;
 
-/*
-char*
-hostname_resolver(const char* hostname, const char* service);
-*/
+static DomainManager* domain_manager;
 
 
 void
@@ -105,7 +103,6 @@ j_connection_pool_init(JConfiguration* configuration)
 
 	//Parameter for fabric init
 	int error;
-	struct fi_info* fi_hints;
 
 	g_return_if_fail(j_connection_pool == NULL);
 
@@ -142,22 +139,14 @@ j_connection_pool_init(JConfiguration* configuration)
 	//Init Libfabric Objects
 	error = 0;
 
-	fi_hints = fi_dupinfo(j_configuration_fi_get_hints(configuration));
-
-	if(fi_hints == NULL)
-	{
-		g_critical("\nAllocating empty hints did not work");
-	}
-
 	//inits j_info
-	//gives a linked list of available provider details into j_info
 	error = fi_getinfo(j_configuration_get_fi_version(configuration),
 										 j_configuration_get_fi_node(configuration),
 										 j_configuration_get_fi_service(configuration),
 										 j_configuration_get_fi_flags(configuration, 0),
-										 fi_hints,
+										 j_configuration_fi_get_hints(configuration),
 										 &j_info);
-	fi_freeinfo(fi_hints); //hints only used for config //TODO test whether stable with no additional fi_hints, but direct read from config
+
 	if(error != 0)
 	{
 		goto end;
@@ -167,21 +156,6 @@ j_connection_pool_init(JConfiguration* configuration)
 		g_critical("\nAllocating Client j_info did not work");
 	}
 
-	//validating juleas needs here
-
-	if(j_info->domain_attr->ep_cnt < (pool->object_len + pool->kv_len + pool->db_len) * pool->max_count + 1)
-	{
-		g_critical("\nDemand for connections exceeds the max number of endpoints available through domain/provider");
-	}
-	/*
-	TODO: activate again
-	if(j_info->domain_attr->cq_cnt < (pool->object_len + pool->kv_len + pool->db_len) * pool->max_count * 2 + 1) //1 active endpoint has 2 completion queues, 1 receive, 1 transmit
-	{
-		g_warning("\nDemand for connections exceeds the optimal number of completion queues available through domain/provider");
-	}
-	*/
-
-
 	//init fabric
 	error = fi_fabric(j_info->fabric_attr, &j_fabric, NULL);
 	if(error != FI_SUCCESS)
@@ -190,27 +164,7 @@ j_connection_pool_init(JConfiguration* configuration)
 		goto end;
 	}
 
-	//init domain
-	error = fi_domain(j_fabric, j_info, &j_domain, NULL);
-	if(error != 0) //WHO THE HELL DID DESIGN THOSE ERROR CODES?! SUCCESS IS SOMETIMES 0, SOMETIMES A DEFINED BUT NOT DOCUMENTED VALUE, ARGH
-	{
-		g_critical("\nAllocating domain on client side did not work\nDetails:\n %s", fi_strerror(error));
-		goto end;
-	}
-	//build event queue for domain
-	error = fi_eq_open(j_fabric, j_configuration_get_fi_eq_attr(configuration), &j_domain_event_queue, NULL);
-	if(error != 0)
-	{
-		g_critical("\nProblem with opening event queue for domain during connection pool init\nDetails:\n %s", fi_strerror(error));
-		goto end;
-	}
-	//bind an event queue to domain
-	error = fi_domain_bind(j_domain, &j_domain_event_queue->fid, 0);
-	if(error != 0)
-	{
-		g_critical("\nProblem with binding event queue to domain during connection pool init\nDetails:\n %s", fi_strerror(error));
-		goto end;
-	}
+	domain_manager = domain_manager_init();
 
 	end:
 	;
@@ -320,8 +274,10 @@ j_endpoint_fini (JEndpoint* endpoint, JMessage* message , gboolean send_shutdown
 		g_critical("\nProblem closing %s-Endpoint event queue.\n Details:\n %s", location, fi_strerror(error));
 		error = 0;
 	}
+
+	domain_unref(endpoint->rc_domain, domain_manager, "client");
+
 	free(endpoint);
-	endpoint = NULL;
 }
 
 /*
@@ -404,20 +360,6 @@ j_connection_pool_fini(void)
 
 	error = 0;
 
-	error = fi_close(&j_domain_event_queue->fid);
-	if(error != 0)
-	{
-		g_critical("\nProblem closing domain event queue on client side.\n Details:\n %s", fi_strerror(error));
-		error = 0;
-	}
-
-	error = fi_close(&j_domain->fid);
-	if(error != 0)
-	{
-		g_critical("\nProblem closing domain on client side.\n Details:\n %s", fi_strerror(error));
-		error = 0;
-	}
-
 	error = fi_close(&j_fabric->fid);
 	if(error != 0)
 	{
@@ -429,15 +371,17 @@ j_connection_pool_fini(void)
 	g_free(pool->kv_queues);
 	g_free(pool->db_queues);
 
+	domain_manager_fini(domain_manager);
+
 	g_slice_free(JConnectionPool, pool);
 
 	g_printf("\nClient shutdown finished\n");
 }
 
-/*
-/
-/ Returns first Element from respective queue or  if none found
-/
+/**
+*
+* Returns first Element from respective queue or  if none found
+*
 */
 static
 gpointer
@@ -458,7 +402,6 @@ j_connection_pool_pop_internal (GAsyncQueue* queue, guint* count, gchar const* s
 	{
 		if(j_endpoint_shutdown_test (endpoint, "pop") == TRUE)
 		{
-			//TODO init a finish of this connection_pool?
 			g_warning("\nShutdown Event present on this endpoint. Server most likely ended, thus no longer available.\n");
 			j_endpoint_fini(endpoint, NULL, FALSE, "pop");
 			goto start;
@@ -497,7 +440,6 @@ j_connection_pool_pop_internal (GAsyncQueue* queue, guint* count, gchar const* s
 			}
 			else
 			{
-				//g_printf("\nInitial sending check succeeded\n\n");
 				reply = j_message_new_reply(message);
 
 				comm_check = j_message_receive(reply, (gpointer) endpoint);
@@ -508,7 +450,7 @@ j_connection_pool_pop_internal (GAsyncQueue* queue, guint* count, gchar const* s
 				}
 				else
 				{
-					//g_printf("\nInitial receiving check succeeded\n\n");
+					//g_printf("\nInitial check succeeded\n\n");
 				}
 			}
 
@@ -592,7 +534,7 @@ j_connection_pool_pop(JBackendType backend, guint index)
 }
 
 
-//TODO deal with sever connection shutdown
+
 void
 j_connection_pool_push(JBackendType backend, guint index, gpointer connection)
 {
@@ -695,7 +637,7 @@ hostname_connector(const char* hostname, const char* service, JEndpoint* endpoin
 		goto end;
 	}
 
-	connection_entry_length = sizeof(struct fi_eq_cm_entry) + 256;
+	connection_entry_length = sizeof(struct fi_eq_cm_entry) + 128;
 
 	for (uint i = 0; i < size; i++)
 	{
@@ -711,10 +653,18 @@ hostname_connector(const char* hostname, const char* service, JEndpoint* endpoin
 		struct fi_eq_cm_entry* connection_entry;
 		uint32_t eq_event;
 
+		RefCountedDomain* rc_domain;
+
 		error = 0;
 
+		if(!domain_request(j_fabric, j_info, j_connection_pool->configuration, &rc_domain, domain_manager))
+		{
+			g_printf("\nDomain request failed on client side.\n");
+			goto end;
+		}
+
 		//Allocate Endpoint and related ressources
-		error = fi_endpoint(j_domain, j_info, &tmp_ep, NULL);
+		error = fi_endpoint(rc_domain->domain, j_info, &tmp_ep, NULL);
 		if(error != 0)
 		{
 			g_critical("\nProblem initing tmp endpoint in resolver on client. \nDetails:\n%s", fi_strerror((int)error));
@@ -726,13 +676,13 @@ hostname_connector(const char* hostname, const char* service, JEndpoint* endpoin
 			g_critical("\nProblem opening tmp event queue in resolver on client. \nDetails:\n%s", fi_strerror((int)error));
 			error = 0;
 		}
-		error = fi_cq_open(j_domain, j_configuration_get_fi_cq_attr(j_connection_pool->configuration), &tmp_cq_transmit, NULL);
+		error = fi_cq_open(rc_domain->domain, j_configuration_get_fi_cq_attr(j_connection_pool->configuration), &tmp_cq_transmit, NULL);
 		if(error != 0)
 		{
 			g_critical("\nProblem opening tmp transmit transmit completion queue on client. \nDetails:\n%s", fi_strerror((int)error));
 			error = 0;
 		}
-		error = fi_cq_open(j_domain, j_configuration_get_fi_cq_attr(j_connection_pool->configuration), &tmp_cq_rcv, NULL);
+		error = fi_cq_open(rc_domain->domain, j_configuration_get_fi_cq_attr(j_connection_pool->configuration), &tmp_cq_rcv, NULL);
 		if(error != 0)
 		{
 			g_critical("\nProblem opening tmp transmit receive completion queue on client. \nDetails:\n%s", fi_strerror((int)error));
@@ -797,12 +747,10 @@ hostname_connector(const char* hostname, const char* service, JEndpoint* endpoin
 			{
 				g_critical("\nClient Problem reading event queue \nDetails:\n%s", fi_strerror(ssize_t_error));
 				ssize_t_error = 0;
-				goto tmp_free;
 			}
 			if (eq_event != FI_CONNECTED)
 			{
 				g_critical("\n\nFI_CONNECTED: %d\neq_event: %d\nClient endpoint did not receive FI_CONNECTED to establish a connection.\n\n", FI_CONNECTED, eq_event);
-				goto tmp_free;
 			}
 			else
 			{
@@ -810,6 +758,7 @@ hostname_connector(const char* hostname, const char* service, JEndpoint* endpoin
 				endpoint->event_queue = tmp_eq;
 				endpoint->completion_queue_transmit = tmp_cq_transmit;
 				endpoint->completion_queue_receive = tmp_cq_rcv;
+				endpoint->rc_domain = rc_domain;
 				ret = TRUE;
 				//g_printf("\nYEAH, CONNECTED EVENT ON CLIENT EVENT QUEUE\n");
 				free(connection_entry);
@@ -818,13 +767,29 @@ hostname_connector(const char* hostname, const char* service, JEndpoint* endpoin
 			free(connection_entry);
 		}
 
-		tmp_free:
 		error = 0;
 		error = fi_close(&tmp_ep->fid);
 		if(error != 0)
 		{
 			g_critical("\nProblem closing tmp endpoint on client\nDetails:\n%s", fi_strerror(error));
 		}
+		error = fi_close(&tmp_cq_rcv->fid);
+		if(error != 0)
+		{
+			g_critical("\nProblem closing tmp endpoint completion queue (receive) on client\nDetails:\n%s", fi_strerror(error));
+		}
+		error = fi_close(&tmp_cq_transmit->fid);
+		if(error != 0)
+		{
+			g_critical("\nProblem closing tmp endpoint completion queue (transmit) on client\nDetails:\n%s", fi_strerror(error));
+		}
+		error = fi_close(&tmp_eq->fid);
+		if(error != 0)
+		{
+			g_critical("\nProblem closing tmp endpoint event queue on client\nDetails:\n%s", fi_strerror(error));
+		}
+
+		domain_unref(rc_domain, domain_manager, "tmp endpoint on client");
 
 		addrinfo = addrinfo->ai_next;
 	}
