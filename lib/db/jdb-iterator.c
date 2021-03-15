5d00d03412b4e147a7e2c8df7f83e405cf14f767
SajadKarim <sajad.karim@gmail.com>
2021-03-15 21:14:49 +0100

    Temporary check-in.

--- lib/db/jdb-iterator.c
+++ lib/db/jdb-iterator.c
@@ -312,7 +312,8 @@ j_db_iterator_get_field_ex(JDBIterator* iterator, gchar const* namespace, gchar 
 					goto _error;
 				}
 
-				if (G_UNLIKELY(!j_bson_iter_find(&iter, name, error)))
+				if (G_UNLIKELY(!(&iter)))
+				//if (G_UNLIKELY(!j_bson_iter_find(&iter, name, error)))
 				{
 					goto _error;
 				}
