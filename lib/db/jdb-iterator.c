056404b37d8b86fb3efd02b8c47991de3cd3a600
SajadKarim <sajad.karim@gmail.com>
2021-03-15 23:42:35 +0100

    Temporary check-in.

--- lib/db/jdb-iterator.c
+++ lib/db/jdb-iterator.c
@@ -315,7 +315,7 @@ j_db_iterator_get_field_ex(JDBIterator* iterator, gchar const* namespace, gchar 
 				if (G_UNLIKELY(!bson_iter_find(&iter, name)))
 				{
 					g_set_error_literal(error, J_BACKEND_BSON_ERROR, J_BACKEND_BSON_ERROR_ITER_KEY_NOT_FOUND, "bson iter can not find key");
-					goto _error;
+					//goto _error;
 				}
 
 				/*if (G_UNLIKELY(!j_bson_iter_value(&iter, J_DB_TYPE_UINT32, &val, error)))
