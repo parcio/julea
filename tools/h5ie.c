7ff077fbb646e242f093f8c5d4bf491cc0e48e28
Timm Leon Erxleben <timm.erxleben@ovgu.de>
2021-12-16 17:05:07 +0100

    format and clean up

--- tools/h5ie.c
+++ tools/h5ie.c
@@ -31,117 +31,116 @@ struct JHDF5CopyParam_t
 };
 
 /**
- * Handle the copy of an object of unknown type.
- * 
- * \param object The object to copy.
- * \param dst_name The name of the new copy.
- * \param copy_data Further parameters such as the destination parent 
- * 					group and the data transfer property list. More
- * 					parameters (like the block size for copying) might
- * 					be added later.
- * 
- * \return A negative value on error.
- */
-static herr_t handle_copy(hid_t object, const gchar* name, JHDF5CopyParam_t* copy_data);
-
-/**
- * Function of type H5L_iterate2_t. Basically a thin wrapper of handle_copy().
- * 
- * This function gets called by H5Literate2().
- * 
- * \param group The group currently iterated.
- * \param name The name of the encountered object.
- * \param info Information of the current link.
- * \param op_data Should be of type JHDF5CopyParam_t.
- * 
- * \return A negative value on error.
- */
-static herr_t iterate_copy(hid_t group, const char* name, const H5L_info2_t* info, void* op_data);
-
-/**
  * Cleanup function for hid_t.
- * 
+ *
  * Wraps H5Idec_ref() which can not handle H5I_INVALID_HID (or other unassigned HDF5 handles).
- * 
+ *
  * \param obj The object to free.
- * 
+ *
  */
 static void hid_cleanup(hid_t* obj);
 
 /**
  * Parse the path of a HDF5 file.
- * 
+ *
  * The path may be prefixed by the name of the corresponding VOL plugin followed by "://".
  * Returns a null terminated GStrv with 2 entries. The first one contains the VOL name and the second one the rest of the path.
  * If no VOL name was given the first entry is set to "native".
- * 
+ *
  * \param path The Path to a HDF5 file (possibly managed by a VOL plugin).
- * 
+ *
  * \return A GStrv as described above or NULL in case of invalid input. Should be freed using g_strfreev().
  */
 static gchar** parse_vol_path(const gchar* path);
 
 /**
  * Creates a file access property list using the given VOL name.
- * 
+ *
  * \param vol The name of the VOL plugin.
- * 
+ *
  * \return The ID of the new file access property list or H5I_INVALID_HID in case of error.
  */
 static hid_t create_fapl_for_vol(const gchar* vol);
 
 /**
+ * Copy the files content to a different file.
+ *
+ * \param src_file The source file.
+ * \param dst_file The destination file.
+ *
+ * \return A negative value on error.
+ */
+static herr_t copy_file(hid_t src_file, hid_t dest_file);
+
+/**
+ * Handle the copy of an object of unknown type.
+ *
+ * \param object The object to copy.
+ * \param dst_name The name of the new copy.
+ * \param copy_data Further parameters such as the destination parent
+ * 					group and the data transfer property list. More
+ * 					parameters (like the block size for copying) might
+ * 					be added later.
+ *
+ * \return A negative value on error.
+ */
+static herr_t handle_copy(hid_t object, const gchar* name, JHDF5CopyParam_t* copy_data);
+
+/**
+ * Copy a dataset to a new location.
+ *
+ * \param set The dataset to copy.
+ * \param dst_loc The location identfier of the target group.
+ * \param dst_name The name of the new copy.
+ * \param dxpl_id The dataset transfer property list.
+ *
+ * \return A negative value on error.
+ */
+static herr_t copy_dataset(hid_t set, hid_t dst_loc, const gchar* dst_name, hid_t dxpl_id);
+
+/**
+ * Function of type H5L_iterate2_t. Basically a thin wrapper of handle_copy().
+ *
+ * This function gets called by H5Literate2().
+ *
+ * \param group The group currently iterated.
+ * \param name The name of the encountered object.
+ * \param info Information of the current link.
+ * \param op_data Should be of type JHDF5CopyParam_t.
+ *
+ * \return A negative value on error.
+ */
+static herr_t iterate_copy(hid_t group, const char* name, const H5L_info2_t* info, void* op_data);
+
+/**
  * Copy an attribute to a new location. Function of type H5A_operator2_t.
- * 
+ *
  * Gets called by H5Aiterate().
- * 
+ *
  * \param location_id The object whose attributes are iterated.
  * \param attr_name The name of the current attribute.
  * \param ainfo Information about the current attribute.
  * \param op_data A pointer to the hid_t of the destination.
- * 
+ *
  * \return A negative value on error.
  */
 static herr_t copy_attribute(hid_t location_id, const char* attr_name, const H5A_info_t* ainfo, void* op_data);
 
 /**
  * Copy all attributes of an object to another object.
- * 
+ *
  * \param src The source object.
  * \param dst The destination object.
- * 
+ *
  * \return A negative value on error.
  */
 static herr_t copy_attributes(hid_t src, hid_t dst);
-
-/**
- * Copy a dataset to a new location.
- * 
- * \param set The dataset to copy.
- * \param dst_loc The location identfier of the target group.
- * \param dst_name The name of the new copy.
- * \param dxpl_id The dataset transfer property list.
- * 
- * \return A negative value on error.
- */
-static herr_t copy_dataset(hid_t set, hid_t dst_loc, const gchar* dst_name, hid_t dxpl_id);
-
-/**
- * Copy the files content to a different file.
- * 
- * \param src_file The source file.
- * \param dst_file The destination file.
- * 
- * \return A negative value on error.
- */
-static herr_t copy_file(hid_t src_file, hid_t dest_file);
 
 /**
  * Print the usage.
  */
 static void usage(void);
 
-// needed to ignore invalid hid which is not done by H5Idec_ref (also to provide correct signature for macro)
 static void
 hid_cleanup(hid_t* obj)
 {
@@ -208,6 +207,153 @@ create_fapl_for_vol(const gchar* vol)
 	}
 
 	return fapl;
+}
+
+static herr_t
+copy_file(hid_t src_file, hid_t dest_file)
+{
+	JHDF5CopyParam_t copy_data;
+	herr_t retval = -1;
+
+	copy_data.dest_curr_group = dest_file;
+	copy_data.dxpl = H5P_DEFAULT;
+	retval = H5Literate(src_file, H5_INDEX_NAME, H5_ITER_NATIVE, NULL, iterate_copy, &copy_data);
+
+	return retval;
+}
+
+static herr_t
+handle_copy(hid_t object, const gchar* name, JHDF5CopyParam_t* copy_data)
+{
+	hid_t tmp_grp;
+	herr_t retval = -1;
+
+	switch (H5Iget_type(object))
+	{
+		case H5I_GROUP:
+			tmp_grp = copy_data->dest_curr_group;
+			if ((copy_data->dest_curr_group = H5Gcreate(tmp_grp, name, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT)) != H5I_INVALID_HID)
+			{
+				if ((retval = H5Literate(object, H5_INDEX_NAME, H5_ITER_NATIVE, NULL, iterate_copy, copy_data)) >= 0)
+				{
+					retval = copy_attributes(object, copy_data->dest_curr_group);
+				}
+
+				H5Gclose(copy_data->dest_curr_group);
+				copy_data->dest_curr_group = tmp_grp;
+			}
+			break;
+
+		case H5I_DATASET:
+			retval = copy_dataset(object, copy_data->dest_curr_group, name, copy_data->dxpl);
+			break;
+
+		case H5I_DATATYPE:
+			/// \todo This could be encountered as well but is not (yet) supported by JULEA. Print error and skip.
+			g_warning("%s: Skipped committed datatype while copying!", G_STRLOC);
+			retval = 0;
+			break;
+
+		case H5I_FILE:
+		case H5I_DATASPACE:
+		case H5I_ATTR:
+		case H5I_UNINIT:
+		case H5I_BADID:
+		case H5I_MAP:
+		case H5I_VFL:
+		case H5I_VOL:
+		case H5I_GENPROP_CLS:
+		case H5I_GENPROP_LST:
+		case H5I_ERROR_CLASS:
+		case H5I_ERROR_MSG:
+		case H5I_ERROR_STACK:
+		case H5I_SPACE_SEL_ITER:
+		case H5I_NTYPES:
+		default:
+			g_critical("%s: Encountered an unexpected object while copying!", G_STRLOC);
+			break;
+	}
+	return retval;
+}
+
+static herr_t
+copy_dataset(hid_t set, hid_t dst_loc, const gchar* dst_name, hid_t dxpl_id)
+{
+	g_auto(hid_t) dst = H5I_INVALID_HID;
+	g_auto(hid_t) space = H5I_INVALID_HID;
+	g_auto(hid_t) dtype = H5I_INVALID_HID;
+	g_auto(hid_t) dapl = H5I_INVALID_HID;
+	g_autofree gchar* buf = NULL;
+	size_t mem_size;
+	herr_t retval = -1;
+
+	// copy general attributes of the set
+	if ((space = H5Dget_space(set)) == H5I_INVALID_HID)
+	{
+		return retval;
+	}
+
+	if ((dtype = H5Dget_type(set)) == H5I_INVALID_HID)
+	{
+		return retval;
+	}
+
+	if ((dapl = H5Dget_access_plist(set)) == H5I_INVALID_HID)
+	{
+		return retval;
+	}
+
+	// determine size of data set in memory
+	if ((mem_size = H5Sget_select_npoints(space) * H5Tget_size(dtype)) <= 0)
+	{
+		return retval;
+	}
+
+	if (!(buf = g_malloc(mem_size)))
+	{
+		return retval;
+	}
+
+	if ((dst = H5Dcreate2(dst_loc, dst_name, dtype, space, H5P_DEFAULT, H5P_DEFAULT, dapl)) == H5I_INVALID_HID)
+	{
+		return retval;
+	}
+
+	if (copy_attributes(set, dst) < 0)
+	{
+		return retval;
+	}
+
+	if (H5Dread(set, dtype, space, space, dxpl_id, buf) < 0)
+	{
+		return retval;
+	}
+
+	if (H5Dwrite(dst, dtype, space, space, dxpl_id, buf) < 0)
+	{
+		return retval;
+	}
+
+	retval = 0;
+	return retval;
+}
+
+static herr_t
+iterate_copy(hid_t group, const char* name, const H5L_info2_t* info, void* op_data)
+{
+	g_auto(hid_t) src_obj = H5I_INVALID_HID;
+	herr_t retval = -1;
+
+	(void)info;
+
+	if ((src_obj = H5Oopen(group, name, H5P_DEFAULT)) == H5I_INVALID_HID)
+	{
+		return retval;
+	}
+
+	retval = handle_copy(src_obj, name, op_data);
+
+	return retval;
 }
 
 static herr_t
@@ -283,158 +429,6 @@ static herr_t
 copy_attributes(hid_t src, hid_t dst)
 {
 	return H5Aiterate(src, H5_INDEX_NAME, H5_ITER_NATIVE, NULL, copy_attribute, &dst);
-}
-
-static herr_t
-copy_dataset(hid_t set, hid_t dst_loc, const gchar* dst_name, hid_t dxpl_id)
-{
-	g_auto(hid_t) dst = H5I_INVALID_HID;
-	g_auto(hid_t) space = H5I_INVALID_HID;
-	g_auto(hid_t) dtype = H5I_INVALID_HID;
-	g_auto(hid_t) dapl = H5I_INVALID_HID;
-	g_autofree gchar* buf = NULL;
-	size_t mem_size;
-	herr_t retval = -1;
-
-	// copy general attributes of the set
-	if ((space = H5Dget_space(set)) == H5I_INVALID_HID)
-	{
-		return retval;
-	}
-
-	if ((dtype = H5Dget_type(set)) == H5I_INVALID_HID)
-	{
-		return retval;
-	}
-
-	if ((dapl = H5Dget_access_plist(set)) == H5I_INVALID_HID)
-	{
-		return retval;
-	}
-
-	// determine size of data set in memory
-	if ((mem_size = H5Sget_select_npoints(space) * H5Tget_size(dtype)) <= 0)
-	{
-		return retval;
-	}
-
-	if (!(buf = g_malloc(mem_size)))
-	{
-		return retval;
-	}
-
-	if ((dst = H5Dcreate2(dst_loc, dst_name, dtype, space, H5P_DEFAULT, H5P_DEFAULT, dapl)) == H5I_INVALID_HID)
-	{
-		return retval;
-	}
-
-	if (copy_attributes(set, dst) < 0)
-	{
-		return retval;
-	}
-
-	if (H5Dread(set, dtype, space, space, dxpl_id, buf) < 0)
-	{
-		return retval;
-	}
-
-	if (H5Dwrite(dst, dtype, space, space, dxpl_id, buf) < 0)
-	{
-		return retval;
-	}
-
-	retval = 0;
-	return retval;
-}
-
-/**
- * \brief Handle copying of an object.
- *
- * \return A negative number on error.
- **/
-static herr_t
-handle_copy(hid_t object, const gchar* name, JHDF5CopyParam_t* copy_data)
-{
-	hid_t tmp_grp;
-	herr_t retval = -1;
-
-	switch (H5Iget_type(object))
-	{
-		case H5I_GROUP:
-			tmp_grp = copy_data->dest_curr_group;
-			if ((copy_data->dest_curr_group = H5Gcreate(tmp_grp, name, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT)) != H5I_INVALID_HID)
-			{
-				if ((retval = H5Literate(object, H5_INDEX_NAME, H5_ITER_NATIVE, NULL, iterate_copy, copy_data)) >= 0)
-				{
-					retval = copy_attributes(object, copy_data->dest_curr_group);
-				}
-
-				H5Gclose(copy_data->dest_curr_group);
-				copy_data->dest_curr_group = tmp_grp;
-			}
-			break;
-
-		case H5I_DATASET:
-			retval = copy_dataset(object, copy_data->dest_curr_group, name, copy_data->dxpl);
-			break;
-
-		case H5I_DATATYPE:
-			/// \todo This could be encountered as well but is not (yet) supported by JULEA. Print error and skip.
-			g_warning("%s: Skipped committed datatype while copying!", G_STRLOC);
-			retval = 0;
-			break;
-
-		case H5I_FILE:
-		case H5I_DATASPACE:
-		case H5I_ATTR:
-		case H5I_UNINIT:
-		case H5I_BADID:
-		case H5I_MAP:
-		case H5I_VFL:
-		case H5I_VOL:
-		case H5I_GENPROP_CLS:
-		case H5I_GENPROP_LST:
-		case H5I_ERROR_CLASS:
-		case H5I_ERROR_MSG:
-		case H5I_ERROR_STACK:
-		case H5I_SPACE_SEL_ITER:
-		case H5I_NTYPES:
-		default:
-			g_critical("%s: Encountered an unexpected object while copying!", G_STRLOC);
-			break;
-	}
-	return retval;
-}
-
-static herr_t
-iterate_copy(hid_t group, const char* name, const H5L_info2_t* info, void* op_data)
-{
-	g_auto(hid_t) src_obj = H5I_INVALID_HID;
-	herr_t retval = -1;
-
-	(void)info;
-
-	if ((src_obj = H5Oopen(group, name, H5P_DEFAULT)) == H5I_INVALID_HID)
-	{
-		return retval;
-	}
-
-	retval = handle_copy(src_obj, name, op_data);
-
-	return retval;
-}
-
-static herr_t
-copy_file(hid_t src_file, hid_t dest_file)
-{
-	JHDF5CopyParam_t copy_data;
-	herr_t retval = -1;
-
-	copy_data.dest_curr_group = dest_file;
-	copy_data.dxpl = H5P_DEFAULT;
-	retval = H5Literate(src_file, H5_INDEX_NAME, H5_ITER_NATIVE, NULL, iterate_copy, &copy_data);
-
-	return retval;
 }
 
 static void
