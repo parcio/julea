547380eedf4d8c338f4cd8f56e5738c0c1b2cf0c
Timm Leon Erxleben <timm.erxleben@ovgu.de>
2021-12-16 13:21:56 +0100

    improve error handling and messages

--- tools/h5ie.c
+++ tools/h5ie.c
@@ -82,22 +82,22 @@ create_fapl_for_vol(const gchar* vol)
 	g_auto(hid_t) connector_id = H5I_INVALID_HID;
 	hid_t fapl = H5I_INVALID_HID;
 
+	g_return_val_if_fail(vol != NULL, H5I_INVALID_HID);
+
 	if ((connector_id = H5VLregister_connector_by_name(vol, H5P_DEFAULT)) == H5I_INVALID_HID)
 	{
-		g_printerr("[ERROR] in %s: Error while openening connector with name \"%s\"!\n", G_STRLOC, vol);
+		g_critical("%s: Error while openening connector with name \"%s\"!\n", G_STRLOC, vol);
 		return H5I_INVALID_HID;
 	}
 
 	if ((fapl = H5Pcopy(H5P_FILE_ACCESS_DEFAULT)) == H5I_INVALID_HID)
 	{
-		g_printerr("[ERROR] in %s: Error while creating file access property list!\n", G_STRLOC);
 		return H5I_INVALID_HID;
 	};
 
 	if (H5Pset_vol(fapl, connector_id, NULL) < 0)
 	{
 		H5Pclose(fapl);
-		g_printerr("[ERROR] in %s: Error while setting VOL connector on file access property list.\n", G_STRLOC);
 		return H5I_INVALID_HID;
 	}
 
@@ -114,64 +114,63 @@ copy_attribute(hid_t location_id, const char* attr_name, const H5A_info_t* ainfo
 	g_auto(hid_t) new_attr = H5I_INVALID_HID;
 	g_autofree gchar* buf = NULL;
 	hid_t dst = *((hid_t*)op_data);
-	herr_t ret = -1;
+	herr_t retval = -1;
 	size_t size, count;
 
 	(void)ainfo;
 
 	if ((attr = H5Aopen(location_id, attr_name, H5P_DEFAULT)) == H5I_INVALID_HID)
 	{
-		goto error;
+		return retval;
 	}
 
 	if ((space = H5Aget_space(attr)) == H5I_INVALID_HID)
 	{
-		goto error;
+		return retval;
 	}
 
 	if ((count = H5Sget_select_npoints(space)) == 0)
 	{
-		goto error;
+		return retval;
 	}
 
 	if ((type = H5Aget_type(attr)) == H5I_INVALID_HID)
 	{
-		goto error;
+		return retval;
 	}
 
 	if ((size = H5Tget_size(type)) == 0)
 	{
-		goto error;
+		return retval;
 	}
 
 	if ((acpl = H5Aget_create_plist(attr)) == H5I_INVALID_HID)
 	{
-		goto error;
+		return retval;
 	}
 
 	if ((new_attr = H5Acreate(dst, attr_name, type, space, acpl, H5P_DEFAULT)) == H5I_INVALID_HID)
 	{
-		goto error;
+		return retval;
 	}
 
 	if (!(buf = g_malloc(size * count)))
 	{
-		goto error;
+		return retval;
 	}
 
 	if (H5Aread(attr, type, buf) < 0)
 	{
-		goto error;
+		return retval;
 	}
 
 	if (H5Awrite(new_attr, type, buf) < 0)
 	{
-		goto error;
+		return retval;
 	}
 
-	ret = 0;
-error:
-	return ret;
+	retval = 0;
+	return retval;
 }
 
 static herr_t
@@ -189,57 +188,57 @@ copy_dataset(hid_t set, hid_t dst_loc, const gchar* dst_name, hid_t dxpl_id)
 	g_auto(hid_t) dapl = H5I_INVALID_HID;
 	g_autofree gchar* buf = NULL;
 	size_t mem_size;
-	herr_t ret = -1;
+	herr_t retval = -1;
 
 	// copy general attributes of the set
 	if ((space = H5Dget_space(set)) == H5I_INVALID_HID)
 	{
-		return ret;
+		return retval;
 	}
 
 	if ((dtype = H5Dget_type(set)) == H5I_INVALID_HID)
 	{
-		return ret;
+		return retval;
 	}
 
 	if ((dapl = H5Dget_access_plist(set)) == H5I_INVALID_HID)
 	{
-		return ret;
+		return retval;
 	}
 
 	// determine size of data set in memory
 	if ((mem_size = H5Sget_select_npoints(space) * H5Tget_size(dtype)) <= 0)
 	{
-		return ret;
+		return retval;
 	}
 
 	if (!(buf = g_malloc(mem_size)))
 	{
-		return ret;
+		return retval;
 	}
 
 	if ((dst = H5Dcreate2(dst_loc, dst_name, dtype, space, H5P_DEFAULT, H5P_DEFAULT, dapl)) == H5I_INVALID_HID)
 	{
-		return ret;
+		return retval;
 	}
 
 	if (copy_attributes(set, dst) < 0)
 	{
-		return ret;
+		return retval;
 	}
 
 	if (H5Dread(set, dtype, space, space, dxpl_id, buf) < 0)
 	{
-		return ret;
+		return retval;
 	}
 
 	if (H5Dwrite(dst, dtype, space, space, dxpl_id, buf) < 0)
 	{
-		return ret;
+		return retval;
 	}
 
-	ret = 0;
-	return ret;
+	retval = 0;
+	return retval;
 }
 
 /**
@@ -251,7 +250,7 @@ static herr_t
 handle_copy(hid_t object, const gchar* name, JHDF5CopyParam_t* copy_data)
 {
 	hid_t tmp_grp;
-	herr_t ret = -1;
+	herr_t retval = -1;
 
 	switch (H5Iget_type(object))
 	{
@@ -259,9 +258,9 @@ handle_copy(hid_t object, const gchar* name, JHDF5CopyParam_t* copy_data)
 			tmp_grp = copy_data->dest_curr_group;
 			if ((copy_data->dest_curr_group = H5Gcreate(tmp_grp, name, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT)) != H5I_INVALID_HID)
 			{
-				if ((ret = H5Literate(object, H5_INDEX_NAME, H5_ITER_NATIVE, NULL, iterate_copy, copy_data)) >= 0)
+				if ((retval = H5Literate(object, H5_INDEX_NAME, H5_ITER_NATIVE, NULL, iterate_copy, copy_data)) >= 0)
 				{
-					ret = copy_attributes(object, copy_data->dest_curr_group);
+					retval = copy_attributes(object, copy_data->dest_curr_group);
 				}
 
 				H5Gclose(copy_data->dest_curr_group);
@@ -270,13 +269,13 @@ handle_copy(hid_t object, const gchar* name, JHDF5CopyParam_t* copy_data)
 			break;
 
 		case H5I_DATASET:
-			ret = copy_dataset(object, copy_data->dest_curr_group, name, copy_data->dxpl);
+			retval = copy_dataset(object, copy_data->dest_curr_group, name, copy_data->dxpl);
 			break;
 
 		case H5I_DATATYPE:
 			/// \todo This could be encountered as well but is not (yet) supported by JULEA. Print error and skip.
-			g_printerr("[WARINING] in %s: Skipped committed datatype while copying!", G_STRLOC);
-			ret = 0;
+			g_warning("%s: Skipped committed datatype while copying!", G_STRLOC);
+			retval = 0;
 			break;
 
 		case H5I_FILE:
@@ -295,10 +294,10 @@ handle_copy(hid_t object, const gchar* name, JHDF5CopyParam_t* copy_data)
 		case H5I_SPACE_SEL_ITER:
 		case H5I_NTYPES:
 		default:
-			g_printerr("%s: Encountered an unexpected object while copying!", G_STRLOC);
+			g_critical("%s: Encountered an unexpected object while copying!", G_STRLOC);
 			break;
 	}
-	return ret;
+	return retval;
 }
 
 /**
@@ -310,37 +309,37 @@ static herr_t
 iterate_copy(hid_t group, const char* name, const H5L_info2_t* info, void* op_data)
 {
 	g_auto(hid_t) src_obj = H5I_INVALID_HID;
-	herr_t ret = -1;
+	herr_t retval = -1;
 
 	(void)info;
 
 	if ((src_obj = H5Oopen(group, name, H5P_DEFAULT)) == H5I_INVALID_HID)
 	{
-		return ret;
+		return retval;
 	}
 
-	ret = handle_copy(src_obj, name, op_data);
+	retval = handle_copy(src_obj, name, op_data);
 
-	return ret;
+	return retval;
 }
 
 static herr_t
 copy_file(hid_t src_file, hid_t dest_file)
 {
 	JHDF5CopyParam_t copy_data;
-	herr_t ret = -1;
+	herr_t retval = -1;
 
 	copy_data.dest_curr_group = dest_file;
 	copy_data.dxpl = H5P_DEFAULT;
-	ret = H5Literate(src_file, H5_INDEX_NAME, H5_ITER_NATIVE, NULL, iterate_copy, &copy_data);
+	retval = H5Literate(src_file, H5_INDEX_NAME, H5_ITER_NATIVE, NULL, iterate_copy, &copy_data);
 
-	return ret;
+	return retval;
 }
 
 static void
 usage(void)
 {
-	printf("usage: h5ie [volname://]source [volname://]target\n");
+	printf("Usage: h5ie [volname://]source [volname://]target\n");
 }
 
 int
@@ -354,56 +353,64 @@ main(int argc, char** argv)
 	g_auto(GStrv) dst_components = NULL;
 	gchar* source = NULL;
 	gchar* destination = NULL;
-	herr_t ret = -1;
+	herr_t retval = -1;
 
 	if (argc <= 2)
 	{
 		usage();
-		return ret;
+		return retval;
 	}
 
 	/// \todo add options (e.g. chunked copy or copy only some objects like h5copy)
 	source = argv[1];
 	destination = argv[2];
 
+	// disable long HDF5 error prints
+	H5Eset_auto2(H5E_DEFAULT, NULL, NULL);
+
 	if ((src_components = parse_vol_path(source)) == NULL)
 	{
-		g_printerr("error parsing\n");
-		return ret;
+		g_critical("%s: Could not parse VOL connector for source file!\n", G_STRLOC);
+		return retval;
 	}
 
 	if ((dst_components = parse_vol_path(destination)) == NULL)
 	{
-		g_printerr("error parsing\n");
-		return ret;
+		g_critical("%s: Could not parse VOL connector for destination file!\n", G_STRLOC);
+		return retval;
 	}
 
 	if ((fapl_vol_src = create_fapl_for_vol(src_components[0])) == H5I_INVALID_HID)
 	{
-		g_printerr("error on fapl src creation\n");
-		return ret;
+		g_critical("%s: Could not create file access property list with given VOL!\n", G_STRLOC);
+		return retval;
 	}
 
 	if ((fapl_vol_dst = create_fapl_for_vol(dst_components[0])) == H5I_INVALID_HID)
 	{
-		g_printerr("error on fapls dst ceration\n");
-		return ret;
+		g_critical("%s: Could not create file access property list with given VOL!\n", G_STRLOC);
+		return retval;
 	}
 
 	if ((src = H5Fopen(src_components[1], H5F_ACC_RDONLY, fapl_vol_src)) == H5I_INVALID_HID)
 	{
-		g_printerr("error on file open\n");
-		return ret;
+		g_critical("%s: Could not open source file!\n", G_STRLOC);
+		return retval;
 	}
 
 	if ((dst = H5Fcreate(dst_components[1], H5F_ACC_TRUNC, H5P_DEFAULT, fapl_vol_dst)) == H5I_INVALID_HID)
 	{
-		g_printerr("error on file create\n");
-		return ret;
+		g_critical("%s: Could not create target file!\n", G_STRLOC);
+		return retval;
 	}
 
-	printf("Copying %s to %s!\n", source, destination);
-	ret = copy_file(src, dst);
+	g_info("Copying %s to %s!\n", source, destination);
+	retval = copy_file(src, dst);
 
-	return ret;
+	if (retval < 0)
+	{
+		g_critical("%s: Could not copy all of the data!\n", G_STRLOC);
+	}
+
+	return retval;
 }
