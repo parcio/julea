0ba453030af57a603645f8419e41c740e20dff1f
Julian Benda <julian.benda@ovgu.de>
2022-05-05 13:54:21 +0200

    Documenting headerfile

--- include/core/jmanagedbackends.h
+++ include/core/jmanagedbackends.h
@@ -19,14 +19,20 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef JULEA_BACKEND_STACK_H
-#define JULEA_BACKEND_STACK_H
-#define JULEA_H ///< \todo remove debug
+/**
+ * \file
+ **/
+
+#ifndef JULEA_MANAGED_BACKENDS_H
+#define JULEA_MANAGED_BACKENDS_H
+
 #if !defined(JULEA_H) && !defined(JULEA_COMPILATION)
-#error "Onyl <julea.h> can be included directly"
+#error "Only <julea.h> can be included directly"
 #endif
 
 #include <glib.h>
+
+G_BEGIN_DECLS
 
 struct JConfiguration;
 typedef struct JConfiguration JConfiguration;
@@ -37,42 +43,81 @@ typedef struct JList JList;
 struct JBackend;
 typedef struct JBackend JBackend;
 
-struct JBackendStackScope;
-typedef struct JBackendStackScope JBackendStackScope;
+/**
+ * \defgroup JManagedBackends
+ *
+ * \todo rename to JManagedObjectBackends?
+ *
+ * A Collection of multiple object backends, which distributed the objects
+ * controlled by a policy Module.
+ *
+ * @{
+ **/
 
-/// storage tier characteristics
-/** \todo more detailed characteristics */
+/**
+ * \defgroup JStorageTier
+ *
+ * \todo evaluate characteristics
+ 
+ * Collection of characteristics about a storage media, used for distributeion
+ * decisions by the policy. 
+ *
+ * \attention all values contained in this may only fixed values
+ * and as this not messuared!
+ * 	
+ * @{
+ **/
+ 	
 struct JStorageTier;
 typedef struct JStorageTier JStorageTier;
 
-/// get estimated storage tier bandwidth
-/** \public \memberof JStorageTier
+/** 
+ * get estimated storage tier bandwidth in bytes per second
+ *
+ * \param[in] this a storage tier  
+ *
  * \return mean of bandwidth in bytes per second
- * */
+ **/
 guint64 j_backend_storage_tier_get_bandwidth(const JStorageTier* this);
 
-/// get estimated storage tier latency
-/** \public \memberof JStorageTier
+/// 
+/** get estimated storage tier latency in us
+ *
+ * \param[in] this a storage tier
+ *
  * \return mean of latency in us
- * */
+ **/
 guint64 j_backend_storage_tier_get_latency(const JStorageTier* this);
 
-/// get estimated total capicity  of storage tier
-/** \public \memberof JStorageTier */
+/// 
+/** get estimated total capacity  of storage tier in bytes
+ *
+ * \param[in] this a storage tier
+ *
+ * \return storage tier capacity in bytes
+ **/
 guint64 j_backend_storage_tier_get_capacity(const JStorageTier* this);
 
-/** \struct JBackendStack backend/jbackendstack.h
- * \ingroup backend
- * \brief Stack of all object backends to allow splitting data between based at a JObjectBackendPolicy
+/**
+ * @}
  */
-struct JBackendStack;
-typedef struct JBackendStack JBackendStack;
 
-/// Possible Access types to log for JObjectBackendPolicy
-/** \ingroup backend
+
+
+struct JManagedBackends;
+typedef struct JManagedBackends JManagedBackends;
+
+/**
+ * Contains data about an ongoing access to a object. 
+ **/
+struct JManagedBackendScope;
+typedef struct JManagedBackendScope JManagedBackendScope;
+
+/**
+ * Possible Access types to log for JObjectBackendPolicy
  * used to signal the usage of their counter part j_object_backend_ functions
  * \sa j_object_backend_policy_process_access
- */
+ **/
 typedef enum
 {
 	J_OBJECT_ACCESS_WRITE,
@@ -82,47 +127,114 @@ typedef enum
 	J_OBJECT_ACCESS_STATUS
 } JObjectBackendAccessTypes;
 
+/**
+ * Policy to distribute objects between object backends in a JManagedBackends.
+ *
+ * process_create, process_access and process_message are used
+ * to signal the policy on different kinds of events.
+ * The implementation of this functions should be minimalistic
+ * to reduce latency for the access, because this functions must terminate
+ * before the access can continued.
+ *
+ * process is executed in a separate thread to provide resources necessary
+ * complex calculation and executing migrations.
+ **/
 typedef struct
 {
-	gboolean (*init)(gpointer* this, const JList* args, JBackendStack* backendStack);
-	gboolean (*fini)(gpointer this);
-	/// short processing period to handle data
-	gboolean (*process_access)(gpointer this,
-				   const gchar* namespace,
-				   const gchar* path,
-				   guint obj_id, ///< [in] object index, used for easier recognise same object
-				   guint tier,	 ///< [in] tier where object is currently on
-				   JObjectBackendAccessTypes access,
-				   gconstpointer data);
-	/// long processing period for migration and large calculation
-	/** \retval FALSE on error */
-	gboolean (*process)(gpointer this);
+	/**
+	 * Policy initializer.
+	 *
+	 * \param[out] policy_data a pointer to store address of policy internal data
+	 * \param[in]  args a list of strings passed from the configuration file
+	 *                  which may used to parametrise the policy.
+	 * \param[in]  backends access to the object backend instances.
+	 **/
+	gboolean (*init)(gpointer* policy_data, const JList* args, JManagedBackends* backends);
+
+	/**
+	 * \param[in] policy_data data at address assigned in init
+	 *            \attention do not forget to free your data if needed
+	 **/
+	gboolean (*fini)(gpointer policy_data);
+
+	/**
+	 * Function used to signal object access to the policy.
+	 *
+	 * \attention This function is called in the same thread as the access is
+	 *            executed, therefore this function should be
+	 *            handled as fast as possible to reduce access latency.
+	 *
+	 * \param[inout] policy_data pointer usage depends on policy
+	 * \param[in] namespace of the object accessed
+	 * \param[in] path of the object accessed
+	 * \param[in] obj_id continuous index which is for  the live time of a object unique
+	 * \param[in] tier identifier for the storage tier
+	 *            the object is currently stored on
+     * \param[in] access type of access performed \sa JObjectBackendAccessType
+	 * \param[in] data corresponding to the access type.
+	 *            For more details see \ref JObjectBackendAccessDetails 
+	 **/
+	gboolean (*process_access)(gpointer policy_data, const gchar* namespace, const gchar* path, guint obj_id, guint tier, JObjectBackendAccessTypes access, gconstpointer data);
+
 	/// short processing period to match new object to a storage tier
 	/** \retval FALSE on error
 	 * \sa j_backend_stack_get_tiers */
-	gboolean (*process_create)(gpointer this,
-				   const gchar* namespace,
-				   const gchar* path,
-				   guint obj_id,      ///< [in] continious object index 
-				   guint* storageTier ///< [out] storage tier ID to store new object
-	);
-	/// process policy custom message
-	/** \retval FALSE on error */
-	gboolean (*process_message)(gpointer this,
-				    const gchar* type, ///< [in] message type encoded as 0-terminated string
-				    gpointer data,     ///< [in,out] optional message data
-				    guint length       ///< [in] size of data field
-	);
+	/**
+	 * Signals a new object creation to the policy, sets instal storage tier.
+	 *
+	 * \param[inout] policy_data pointer usage depends on policy
+	 * \param[in] namespace of the object which will be created
+	 * \param[in] path of the object which will be created
+	 * \param[in] obj_id \ref process_access
+	 * \param[out] storage_tier ID of storage tier to create object on
+	 **/
+	gboolean (*process_create)(gpointer policy_data, const gchar* namespace, const gchar* path, guint obj_id, guint* storage_tier);
 
-	gpointer data;
+	/**
+	 * handle messages send to policy.
+	 *
+	 * \todo add reply functionality
+	 *
+	 * \param[inout] policy_data pointer usage depends on policy
+	 * \param[in] type message type encoded as 0-terminated string
+	 * \param[inout] data optional message data
+	 * \param[in] size of data field
+	 **/
+	gboolean (*process_message)(gpointer policy_data, const gchar* type, gpointer data, guint length);
+
+	/**
+	 * Function use to pass a dedicated thread for migrations
+	 * and calculations to policy.
+	 **/
+	gboolean (*process)(gpointer policy_data);
+
+
+	gpointer data; ///< reference to data allocated in init 
 } JObjectBackendPolicy;
 
 JObjectBackendPolicy* backend_policy_info(void);
 
+/**
+ * \defgroup JObjectBackendAccessDetails 
+ *
+ * Structs used to pass additional data on a
+ * JObjectBackendPolicy.process_access  call
+ *
+ * @{
+ *
+ **/
+
+/**
+ * Additional data for a read and write access
+ **/
 struct JObjectBackendRWAccess {
-	guint64 offset;
-	guint64 length;
+	guint64 offset; ///< access offset 
+	guint64 length; ///< length of data to be written/read 
 };
+
+/**
+ * @}
+ **/
 
 gboolean j_backend_stack_init(
 	JConfiguration* config,
@@ -245,5 +357,10 @@ j_backend_stack_get_by_prefix(JBackendStack* this, gchar const* namespace, gchar
 gboolean
 j_backend_stack_iterate(JBackendStack* this, gpointer iterator, gchar const** name);
 
+/**
+ * @}
+ **/
+
+G_END_DECLS
 
 #endif
