b1f1546773d530d8fd071a97c1151b473dc217a7
Julian Benda <julian.benda@ovgu.de>
2022-07-02 13:03:29 +0200

    Add License for jmanagedeobjectbackend

--- include/core/jmanagedbackends.h
+++ include/core/jmanagedbackends.h
@@ -1,27 +1,24 @@
-/** \file JManagedBackends.h
+/** 
+ * \file JManagedBackends.h
  * \ingroup backend
  *
  * \copyright
  * JULEA - Flexible storage framework
- * Copyright (C) 2010-2021 Michael Kuhn
- * \n
+ * Copyright (C) 2022 Julian Benda
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * \n
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * \n
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-/**
- * \file
- **/
 
 #ifndef JULEA_MANAGED_BACKENDS_H
 #define JULEA_MANAGED_BACKENDS_H
--- lib/core/jmanagedbackends.c
+++ lib/core/jmanagedbackends.c
@@ -1,3 +1,25 @@
+/**
+ * \file JManagedBackends.c
+ * \ingroup backend
+ *
+ * \copyright
+ * JULEA - Flexible storage framework
+ * Copyright (C) 2022 Julian Benda
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
 #include <bits/types/struct_timeval.h>
 #include <core/jmanagedbackends.h>
 #include <core/jbackend.h>
