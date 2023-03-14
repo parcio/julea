77df28116b4a0c03f5a05871cb0e4f1ff0f0806b
Julian Benda <julian.benda@ovgu.de>
2023-03-14 14:32:34 +0100

    Code formatting

--- scripts/decission-support.R
+++ scripts/decission-support.R
@@ -21,10 +21,9 @@ suppressMessages(library(plotly))
 suppressMessages(library(dplyr))
 suppressMessages(library(scales))
 suppressMessages(library(ggpubr))
-library(reshape2)
 
 options(knitr.kable.NA = "")
-options(warn=1)
+options(warn = 1)
 
 first_round <- TRUE
 last_frame <- NA
@@ -60,11 +59,20 @@ test_validity <- function(data) {
   return(res)
 }
 
-
 hovertemplate <- "%{text}<extra></extra>"
-
+replace_col_names <- function(dfs, old, new) {
+  old_new <- data.frame(old, new)
+  dfs <- lapply(dfs, function(df) {
+    apply(old_new, 1, function(x) {
+      names(df)[which(names(df) == x[1])] <<- x[2]
+      return(df)
+    })
+    return(df)
+  })
+  return(dfs)
+}
 # create duration plot and table for one backend type
-plot_durations <- function(data, key,...,format="simple") {
+plot_durations <- function(data, key, ..., format = "simple") {
 
   # generate shorted backend label
   # removes path if backend only appears once
@@ -78,7 +86,7 @@ plot_durations <- function(data, key,...,format="simple") {
   # analyse for runtime per operation type
   sepperate_data <- data %>%
     group_by(operation, backend) %>%
-    summarise(duration=sum(duration), cnt=n(), .groups="drop_last") %>%
+    summarise(duration = sum(duration), cnt = n(), .groups = "drop_last") %>%
     group_modify(function(df, keys) {
       df$speed_up <- max(df$duration) / df$duration
       df$time_diff <- max(df$duration) - df$duration
@@ -89,17 +97,21 @@ plot_durations <- function(data, key,...,format="simple") {
   unit <- "s"
   if (max(sepperate_data$duration) < 10.) {
     sepperate_data$duration <- sepperate_data$duration * 1000.
+    data$duration <- data$duration * 1000.
     unit <- "ms"
   }
   label <- labs(y = paste0("time in ", unit), fill = "backend", x = "")
 
   # plotting
-  # sort categories after longest execution time
+  # sort categories after largest time save
   sepperate_data$backend <- as.factor(sepperate_data$backend)
-  sepperate_data$operation <- reorder(factor(sepperate_data$operation), sepperate_data$time_diff, FUN=max, decreasing=TRUE, na.rm = TRUE)
+  sepperate_data$operation <- reorder(
+    factor(sepperate_data$operation),
+    sepperate_data$time_diff,
+    FUN = max, decreasing = TRUE, na.rm = TRUE)
   off <- 0
   sepperate_data <- sepperate_data %>%
-    group_modify(function(df, keys){
+    group_modify(function(df, keys) {
       df$base <- off
       off <<- df$duration + off
       return(df)
@@ -107,7 +119,12 @@ plot_durations <- function(data, key,...,format="simple") {
   sepperate_data <- arrange(sepperate_data, operation)
 
   # static plot
-  operation_factor <- factor(sepperate_data$operation,level=levels(sepperate_data$operation),labels=paste(unique(sepperate_data$operation), "\n#", unique(sepperate_data$cnt)))
+  operation_factor <- factor(
+    sepperate_data$operation,
+    level = levels(sepperate_data$operation),
+    labels = paste(
+      unique(sepperate_data$operation),
+      "\n#", unique(sepperate_data$cnt)))
   seperate <- ggplot(sepperate_data,
       aes(y = duration, x = operation_factor, fill = backend)) +
     geom_col(position = "dodge") +
@@ -116,33 +133,44 @@ plot_durations <- function(data, key,...,format="simple") {
     label
 
   # inteacrtive plotting
+  text_format <- ~sprintf(
+      "<b>%s</b><br><br>number: %s<br>duration: %.2f%s<br>avg: %.2f%s",
+      backend, cnt, duration, unit, duration / cnt, unit)
   sepperate_ly <- plot_ly(
-    data=sepperate_data,
-    x=operation_factor,
-    y=~duration,
-    color=~backend, colors=hue_pal()(length(unique(sepperate_data$backend))),
-    text=~sprintf("<b>%s</b><br><br>number: %s<br>duration: %.2f%s<br>avg: %.2f%s",
-          backend, cnt, duration, unit, duration / cnt, unit),
-    type="bar", showlegend=TRUE, orientation='v', hovertemplate=hovertemplate, textposition="none") %>%
-    layout(xaxis=list(categoryorder="array", categoryarray=levels(operation_factor)))
+      data = sepperate_data,
+      x = operation_factor, y = ~duration,
+      color = ~backend,
+      colors = hue_pal()(length(unique(sepperate_data$backend))),
+      text = text_format, textposition = "none", hovertemplate = hovertemplate,
+      type = "bar") %>%
+    layout(
+      xaxis = list(
+        categoryorder = "array",
+        categoryarray = levels(operation_factor)))
   total_ly <- plot_ly(
-    data=sepperate_data,
-    x=~backend, y=~duration, base=~base, color=~operation,
-    colors=dichromat_pal("DarkRedtoBlue.12")(length(unique(sepperate_data$operation))),
-    type="bar", showlegend=TRUE, offsetgroup=0, orientation='v', hovertemplate=hovertemplate,
-    text=~sprintf("<b>%s</b><br><br>number: %s<br>duration: %.2f%s", operation, cnt, duration, unit), textposition="none") %>%
-    layout(xaxis=list(categoryorder="array", categoryarray=levels(sepperate_data$backend)))
-  
+      data = sepperate_data,
+      x = ~backend, y = ~duration, base = ~base,
+      color = ~operation,
+      colors = ~dichromat_pal("DarkRedtoBlue.12")(length(unique(operation))),
+      text = text_format, textposition = "none", hovertemplate = hovertemplate,
+      type = "bar", offsetgroup = 0) %>%
+    layout(
+      xaxis = list(
+        categoryorder = "array",
+        categoryarray = levels(sepperate_data$backend)))
+
   # table
   sepperate_data <- sepperate_data %>%
     group_modify(function(df, keys) {
       cnt <- sprintf("#%s", df$cnt[1])
       df$cnt <- sprintf("%.2f", df$duration / df$cnt)
-      df <- df %>% arrange(duration) %>% add_row(cnt=cnt, .before=0)
+      df <- df %>% arrange(duration) %>% add_row(cnt = cnt, .before = 0)
       return(df)}) %>%
     ungroup()
-  sepperate_data[! is.na(sepperate_data$duration),]$operation = NA # clean nonsens values
-  sepperate_data <- subset(sepperate_data, select=-c(base)) # remove column with plotting only values
+  # clean nonsens values
+  sepperate_data[! is.na(sepperate_data$duration), ]$operation <- NA
+  # remove column with plotting only values
+  sepperate_data <- subset(sepperate_data, select = -c(base))
 
   # analyse for total runtime
   total_data <- data %>%
@@ -158,26 +186,50 @@ plot_durations <- function(data, key,...,format="simple") {
   total_data$time_diff <- max(total_data$duration) - total_data$duration
   total_data <- arrange(total_data, duration)
 
-  # rename cols for nicer table headers TODO nicer
-  colnames(total_data)[which(names(total_data) == "duration")] <- sprintf("duration (%s)", unit)
-  colnames(sepperate_data)[which(names(sepperate_data) == "duration")] <- sprintf("duration (%s)", unit)
-  colnames(total_data)[which(names(total_data) == "time_diff")] <- sprintf("time diff (%s)", unit)
-  colnames(sepperate_data)[which(names(sepperate_data) == "time_diff")] <- sprintf("time diff (%s)", unit)
-  colnames(sepperate_data)[which(names(sepperate_data) == "cnt")] <- sprintf("cnt/avg duration (%s)", unit)
-
   # generate HTML/text table with links
   plot_file_name <- paste(key[1], "time.svg", sep = "_")
   plot_html_file_name <- paste(key[1], "time.html", sep = "_")
-  table_sepperate_str <- knitr::kable(sepperate_data, format, digits=2, table.attr = "class=\"fancy\"")
-  table_total_str <- knitr::kable(total_data, format, digits=2, table.attr = "class=\"fancy\"")
+  renamed <- replace_col_names( # rename column for table
+    list(total_data = total_data, sepperate_data = sepperate_data),
+    c("duration", "time_diff", "speed_up"),
+    c(sprintf("duration (%s)", unit),
+      sprintf("time diff (%s)", unit),
+      "speed up")
+  )
+  table_sepperate_str <- knitr::kable(
+      renamed$sepperate_data,
+      format, digits = 2, table.attr = "class=\"fancy\"")
+  table_total_str <- knitr::kable(
+      renamed$total_data, format,
+      digits = 2, table.attr = "class=\"fancy\"")
+
   if (format == "html") {
-    cat("<h2 id=\"",as.character(key[1]),"\">",as.character(key[1]),"</h2>", sep = "")
-    cat("<table><tbody><tr><td rowspan=\"2\">",table_sepperate_str,"</td><td style=\"height:10px\">", table_total_str,"</td></tr><tr><td style=\"vertical-align:top\">",
-      "<a href=\"./",plot_html_file_name,"\" style=\"display: inline-block\"><object data=\"./", plot_file_name,"\" alt=\"barplot of results listed in table\" style=\"width:100%;pointer-events: none\"></object></a>",
-      # "<iframe width=\"1080px\" height=\"720px\" src=\"./",plot_html_file_name,"\" title=\"inteactive barplot for result listed in tables\"></iframe>",
+    cat(
+      "<h2 id=\"", as.character(key[1]), "\">",
+      as.character(key[1]),
+      "</h2>", sep = "")
+    cat(
+      "<table><tbody><tr><td rowspan=\"2\">",
+      table_sepperate_str,
+      "</td><td style=\"height:10px\">",
+      table_total_str,
+      "</td></tr><tr><td style=\"vertical-align:top\">",
+      "<a href=\"./",
+      plot_html_file_name,
+      "\" style=\"display: inline-block\">",
+      "<object data=\"./",
+      plot_file_name,
+      "\" alt=\"barplot of results listed in table\"",
+      "style=\"width:100%;pointer-events: none\"></object></a>",
       "</td></tr></tbody></table>", sep = "")
   } else {
-    cat(as.character(key[1]), paste0("plot: ", plot_file_name), "", table_total_str, "", table_sepperate_str, "", "", sep="\n")
+    cat(
+      as.character(key[1]),
+      paste0("plot: ",
+        plot_file_name), "",
+        table_total_str, "",
+        table_sepperate_str, "",
+        "", sep = "\n")
   }
 
   # combining static plots and store them
@@ -186,21 +238,24 @@ plot_durations <- function(data, key,...,format="simple") {
     common.legend = TRUE, legend = "bottom")
   ggsave(plot_file_name, plot,
     unit = "in", width = 11, height = 6, bg = "white")
-  
+
   # combine interactive plots and store them
-  N = length(unique(sepperate_data$backend))-1
+  n <- length(unique(sepperate_data$backend)) - 1
   htmlwidgets::saveWidget(
     subplot(
       sepperate_ly,
       total_ly,
       widths = c(0.8, 0.2)) %>%
     layout(
-      font=list(size=18),
-      yaxis=list(title=label$y),
-      hoverlabel=list(bgcolor="white"),
-      barmode="group",
-      legend=list(traceorder="normal")) %>%
-        htmlwidgets::onRender(sprintf(readLines(paste(script_dir, "interactive.js", sep="/")), N)),
+      font = list(size = 18),
+      yaxis = list(title = label$y),
+      hoverlabel = list(bgcolor = "white"),
+      barmode = "group",
+      legend = list(traceorder = "normal")) %>%
+    htmlwidgets::onRender(
+      sprintf(
+        read_file("interactive.js"),
+        n)),
     plot_html_file_name,
     selfcontained = FALSE,
     libdir = "lib")
@@ -210,6 +265,9 @@ plot_durations <- function(data, key,...,format="simple") {
 # setup, load data
 args <- commandArgs(trailingOnly = FALSE)
 script_dir <- dirname(sub("--file=", "", args[grep("--file=", args)]))
+read_file <- function(name) {
+  return(readLines(paste(script_dir, name, sep = "/")))
+}
 args <- commandArgs(trailingOnly = TRUE)
 if (length(args) < 1) {
   stop(
@@ -217,7 +275,8 @@ if (length(args) < 1) {
       "usage:",
       "script", # TODO check if script name can be determined
       "<summary.csv> <- generated by decission-support.sh",
-      "[(print|html)] <- output as simple print(default) or as html", sep="\n"),
+      "[(print|html)] <- output as simple print(default) or as html",
+      sep = "\n"),
     call. = FALSE)
 }
 data <- read.csv(args[1])
@@ -233,16 +292,21 @@ if (res == FALSE) {
 
 # print html header
 if (as_html) {
-  cat("<!DOCTYPE html><html lang=\"en\">")
-  cat("<head><style>",paste(readLines(paste(script_dir, "style.css", sep="/")), collapse = "\n"),"</style></head><body><header>", sep="")
-  cat("<nav><ul>")
-  for ( backend in unique(data$backend)) {
-    cat("<li><a href=\"#",backend,"\">", backend,"</a></li>", sep="")
+  cat(
+    "<!DOCTYPE html><html lang=\"en\">",
+    "<head><style type=\"text/css\">",
+    paste(read_file("style.css"), collapse = "\n"),
+    "</style></head><body><header>",
+    "<nav><ul>", sep = "")
+  for (backend in unique(data$backend)) {
+    cat("<li><a href=\"#", backend, "\">", backend, "</a></li>", sep = "")
   }
   cat("</ul></nav></header>")
 }
 
-data %>% group_by(backend) %>% group_walk(plot_durations, format=if(as_html) "html" else "simple")
+data %>%
+  group_by(backend) %>%
+  group_walk(plot_durations, format = if (as_html) "html" else "simple")
 
 # print html closing
 if (as_html) {
--- scripts/interactive.js
+++ scripts/interactive.js
@@ -1,12 +1,28 @@
+
 function(el) {
+  axis_adapt = function(axis, original_axis, id, dir) {
+    range = axis.range
+    range[1] += dir
+
+    label = original_axis[id]
+    categoryarray = axis.categoryarray
+    if(dir < 1) {
+      categoryarray.splice(categoryarray.indexOf(label), 1)
+    } else {
+      i = 0
+      while(i < categoryarray.length && original_axis.indexOf(categoryarray[i]) < id) {  i += 1  }
+      categoryarray.splice(i, 0, label)
+    }
+    return [range, categoryarray]
+  }
   N=%.0f
   el.on('plotly_legendclick', function(d) { 
     // store original configuration
     if(!el.mystore) {
       el.mystore = {}
       el.mystore.autorange = true
-      el.mystore.labels = JSON.parse(JSON.stringify(d.layout.xaxis2.categoryarray))
       el.mystore.labels0 = JSON.parse(JSON.stringify(d.layout.xaxis.categoryarray))
+      el.mystore.labels = JSON.parse(JSON.stringify(d.layout.xaxis2.categoryarray))
     }
     // get plotting node
     node = d.node;
@@ -21,21 +37,8 @@ function(el) {
 
     // click at time/operation plot
     if (d.curveNumber < N) { 
-      // modify x-axis range
-      range = d.layout.xaxis2.range
-      range[1] += dir
+      [range, categoryarray] = axis_adapt(d.layout.xaxis2, el.mystore.labels, d.curveNumber, dir)
       layout["xaxis2.range"] = range,
-
-      // modify x-axis label
-      label = el.mystore.labels[d.curveNumber]
-      categoryarray = d.layout.xaxis2.categoryarray
-      if (dir < 1) { // removeLabel
-        categoryarray.splice(categoryarray.indexOf(label), 1)
-      } else { // add label
-        i = 0
-        while(i < categoryarray.length && el.mystore.labels.indexOf(categoryarray[i]) < d.curveNumber) { i += 1}
-        categoryarray.splice(i, 0, label)
-      }
       layout["xaxis2.categoryarray"] = categoryarray
 
       if (el.mystore.autorange) { // adapt y axis for plot
@@ -67,24 +70,9 @@ function(el) {
       Plotly.update(node, {"visible": dir > 0 ? true : "legendonly"}, layout, [d.curveNumber])
 
     } else { // click in time/backend plot
-      // modify x-axis range
-      range = d.layout.xaxis.range
-      range[1] += dir
+      [range, categoryarray] = axis_adapt(d.layout.xaxis, el.mystore.labels0, d.curveNumber - N, dir)
       layout["xaxis.range"] =  range,
-      
-      // modify x-axis labels
-      label = el.mystore.labels0[d.curveNumber - N]
-      console.log("label", label)
-      categoryarray = d.layout.xaxis.categoryarray
-      if (dir < 1) {
-        categoryarray.splice(categoryarray.indexOf(label), 1)
-      } else {
-        i = 0 
-        while(i < categoryarray.length && el.mystore.labels0.indexOf(categoryarray[i]) < d.curveNumber - N) { i+= 1}
-        categoryarray.splice(i, 0, label)
-      }
       layout["xaxis.categoryarray"] =  categoryarray
-      console.log(categoryarray)
 
       basis = []
       mod = []
@@ -97,10 +85,11 @@ function(el) {
 
       if (el.mystore.autorange) { // adapt y-axis forPlot
         max = 0
-        for(i = 0; i < d.data.length; i += 1) {
+        for(i = N; i < d.data.length; i += 1) {
           visible = d.fullData[i].visible != "legendonly" && i != d.curveNumber
+          if (!visible) { continue }
           for(j = 0; j < d.layout.xaxis2.categoryarray.length; j += 1) {
-            if(visible && d.layout.xaxis2.categoryarray.indexOf(d.data[i].x[j]) != -1) {
+            if(d.layout.xaxis2.categoryarray.indexOf(d.data[i].x[j]) != -1) {
               if (i >= d.curveNumber) {
                 max = Math.max(max, basis[i-d.curveNumber][j] + d.data[i].y[j])
               } else {
@@ -111,14 +100,14 @@ function(el) {
         }
         layout["yaxis2.range"] = [-max*0.01, max*1.01]
         max = 0
-        for(i = 0; i < categoryarray.length; i += 1) {
+        categoryarray.forEach(category => {
           for (j = 0; j < N; j += 1) {
             if (d.layout.xaxis2.categoryarray.indexOf(d.data[j].name) == -1) { continue; }
-            idx = d.data[j].x.indexOf(categoryarray[i])
+            idx = d.data[j].x.indexOf(category)
             y = d.data[j].y[idx]
             max = Math.max(max, y)
           }
-        }
+        })
         layout["yaxis.range"] = [-max*0.01, max*1.01]
       }
       Plotly.update(node, {base: basis}, layout, mod)
