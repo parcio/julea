aafdbd75bbd8870a3a47b91f3f036d1705e29851
Finn Hering <finn@hering.sh>
2025-02-24 21:52:41 +0100

    fix: add missing LD_LIBRARY_PATH and fix bin path

--- docker-spack-entrypoint.sh
+++ docker-spack-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-export PATH=$PATH:/app/julea-install
+export PATH=$PATH:/app/julea-install/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/app/julea-install/lib
 . /app/scripts/environment.sh
 
 # Run CMD
