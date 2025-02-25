523080ac9cf5aae0792cb29566fa479e3628924a
Finn Hering <finn@hering.sh>
2025-02-25 18:20:46 +0100

    fix: typo

--- docker-spack-entrypoint.sh
+++ docker-spack-entrypoint.sh
@@ -2,7 +2,7 @@
 
 export PATH=$PATH:/app/julea-install/bin
 
-find "$/app/julea-install/lib" -type d | while read -r dir; do
+find "/app/julea-install/lib" -type d | while read -r dir; do
   export LD_LIBRARY_PATH="$dir:$LD_LIBRARY_PATH"
 done
 
